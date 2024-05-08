a5d89184a4d55876514818fa72019e91a8eee005
Jbalint <91829380+JonisBalint@users.noreply.github.com>
2024-05-08 13:56:19 +0200

    Add files via upload

---
+++ LN40BG_0503/2.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $("#1").click(function(){
+      $(".1").hide();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#2").click(function(){
+      $(".2").hide();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#3").click(function(){
+      $(".3").hide();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#4").click(function(){
+      $(".4").hide();
+    });
+  });
+
+  $(document).ready(function(){
+    $("#5").click(function(){
+      $(".5").hide();
+    });
+  });
