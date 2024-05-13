5b5b02b6abf26ce88730e1497cf60fc415d9ff1a
Jbalint <91829380+JonisBalint@users.noreply.github.com>
2024-05-13 14:06:42 +0200

    Add files via upload

---
+++ LN40BG_0503/LN40BG_7.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $('#szoveg').click(function() {
+        $('#box').append('<p>Mérnökinformatikus</p>');
+    });
+
+    $('#urlap_fejlec').click(function() {
+        $('#form_fejlec').text('ŰRLAP-LN40BG');
+    });
+
+    $('#fejlec').click(function() {
+        $('#h1').append('jQuery feladat');
+    });
+    
+    $('#alcim').click(function() {
+        $('#h2').append('HTML - Add elements');
+    });
+
+    $('#uj_gomb').click(function() {
+        $('#gombbeilleszt').append('<button>ME GEIK-MI</button>');
+    });
+
+    $('#gomb').click(function() {
+        document.getElementById("gomb").prepend("MI ");
+    });
+});
